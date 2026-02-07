7e1be58fdce05d4767543e6b2d3ff957d8dff1b8
Helton Ribeiro <oiheltong@gmail.com>
2026-02-07 15:05:20 -0300

    Adição do botão Back To Top

---
+++ assets/js/hero.js
@@ -0,0 +1,30 @@
+// BACK TO TOP
+const backToTopElement = document.getElementById("back-to-top-button");
+const elementToObserve = document.querySelector(".heltonRibeiro h1");
+
+const handleBackToTop = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+};
+
+backToTopElement.addEventListener("click", handleBackToTop);
+
+const options = {
+    root: null,
+    threshold: 0
+};
+
+const handleBackToTopVisibility = (entries) => {
+    entries.forEach((entry) => {
+        if (!entry.isIntersecting) {
+            backToTopElement.classList.add("back-to-top-visible");
+        } else {
+            backToTopElement.classList.remove("back-to-top-visible");
+        }
+    });
+};
+
+const observer = new IntersectionObserver(handleBackToTopVisibility, options);
+
+if (elementToObserve) {
+    observer.observe(elementToObserve);
+}
