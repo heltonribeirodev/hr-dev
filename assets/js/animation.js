fe0dfc12a573179603c468790a4bd0a3de934a60
Helton Ribeiro <oiheltong@gmail.com>
2026-02-16 20:09:30 -0300

    Animações

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -3,32 +3,30 @@ gsap.registerPlugin(ScrollTrigger);
 const tl = gsap.timeline(); 
 
 const textoAnimado = document.querySelectorAll(".textoAnimado");
-const splitType = new SplitType(textoAnimado, {type: "chars"});
+const splitType = new SplitType(textoAnimado, { type: "chars" });
 
 const textoWords = document.querySelectorAll(".textoAnimadoWords");
-const splitTypeWords = new SplitType(textoWords, {type: "words"})
-
+const splitTypeWords = new SplitType(textoWords, { type: "words" });
 
 tl.from(".etiqueta", {
     opacity: 0,
     y: -20,
     duration: 0.4
-})
+});
 
 tl.from(".divLogos", {
     opacity: 0,
-    y:-5,
-    duration:0.5
-})
+    y: -5,
+    duration: 0.5
+});
 
 tl.from(splitType.chars, {
     opacity: 0,
     filter: "blur(5px)",
-    x:-20,
-    stagger:0.1,
+    x: -20,
+    stagger: 0.1,
     duration: 0.2
-})
-
+});
 
 tl.from(splitTypeWords.words, {
     opacity: 0,
@@ -39,31 +37,25 @@ tl.from(splitTypeWords.words, {
 
 tl.from(".seta", {
     opacity: 0,
-    filter:"blur(10px)",
+    filter: "blur(10px)",
     y: -20,
-    duration:.5
-})
-
-// Animations Cards
-const observerLeft = document.querySelectorAll('.hiddenLeft');
-const observerRight = document.querySelectorAll('.hiddenRight');
-
-// 1. PRIMEIRO: Defina o observador
-const myObserverLeft = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if(entry.isIntersecting){
-            entry.target.classList.add('showLeft');
-        }
-    });
+    duration: 0.5
 });
 
-const myObserverRight = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if(entry.isIntersecting){
-            entry.target.classList.add('showRight');
+
+// Aniations Cards
+
+const cardObserver = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+            cardObserver.unobserve(entry.target);
         }
     });
+}, {
+    threshold: 0.2
 });
 
-observerLeft.forEach((el) => myObserverLeft.observe(el));
-observerRight.forEach((el) => myObserverRight.observe(el));
+document
+    .querySelectorAll('.hiddenLeft, .hiddenRight')
+    .forEach(el => cardObserver.observe(el));
